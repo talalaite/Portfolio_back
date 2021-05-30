854bfb7cb081a13de949841a9f1182f317502d6b
talalaite <justina.talalaite@gmail.com>
2021-05-31 01:42:21 +0300

    authorization?

--- src/middleware.js
+++ src/middleware.js
@@ -4,7 +4,8 @@ const { jwtSecret } = require("./config");
 module.exports = {
   loggedIn: (req, res, next) => {
     try {
-      const token = req.headers.authorization?.split(" ")[1];
+      // kodel po authorization ?
+      const token = req.headers.authorization.split(" ")[1];
       const decodedToken = jwt.verify(token, jwtSecret);
       req.userData = decodedToken;
       next();
